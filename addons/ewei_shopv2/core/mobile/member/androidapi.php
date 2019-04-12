4f74e8a2a4643a65b93b76dccf6e7e19697b6c2f
xiaozhi9752 <975247129@qq.com>
2019-04-12 19:13:44 +0800

    1

--- addons/ewei_shopv2/core/mobile/member/androidapi.php
+++ addons/ewei_shopv2/core/mobile/member/androidapi.php
@@ -2141,7 +2141,7 @@ class Androidapi_EweiShopV2Page extends MobilePage
 		// show_json($data);
 		$result = pdo_update("ewei_shop_member", $data, array('openid' => $_W['openid'], 'uniacid' => $_W['uniacid']));
 
-		if ($result) {
+		if ($result !== false) {
 			returnJson(array());
 		} else {
 			returnJson(array(), '失败', -1);
