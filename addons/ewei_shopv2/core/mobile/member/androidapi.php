1361bbf89646f82b19bcfe343fb167667e316e9a
dengjiangg <2856986090@qq.com>
2019-04-18 15:23:12 +0800

    update

--- addons/ewei_shopv2/core/mobile/member/androidapi.php
+++ addons/ewei_shopv2/core/mobile/member/androidapi.php
@@ -937,7 +937,7 @@ class Androidapi_EweiShopV2Page extends MobilePage
 			$id     = $_GPC['id']; //订单ID
 			$op     = $_GPC['op']; //特殊参数
 			$openid = $_W['openid'];
-			$sell = pdo_fetch("select g.*,m.nickname,m.mobile,m.zfbfile,m.wxfile,m.bankid,m.bankname,m.bank,m2.nickname as nickname2,m2.mobile as mobile2,m2.zfbfile as zfbfile2,m2.wxfile as wxfile2,m2.bankid as bankid2,m2.bankname as bankname2,m2.bank as bank2 from" . tablename('guamai') . ' g left join ' . tablename('ewei_shop_member') . ' m ON m.openid=g.openid left join ' . tablename('ewei_shop_member') . ' m2 ON m2.openid=g.openid2 ' . " where g.uniacid=" . $_W['uniacid'] . " and g.id='$id'");		
+			$sell   = pdo_fetch("select g.*,m.nickname,m.mobile,m.zfbfile,m.wxfile,m.bankid,m.bankname,m.bank,m2.nickname as nickname2,m2.mobile as mobile2,m2.zfbfile as zfbfile2,m2.wxfile as wxfile2,m2.bankid as bankid2,m2.bankname as bankname2,m2.bank as bank2 from" . tablename('guamai') . ' g left join ' . tablename('ewei_shop_member') . ' m ON m.openid=g.openid left join ' . tablename('ewei_shop_member') . ' m2 ON m2.openid=g.openid2 ' . " where g.uniacid=" . $_W['uniacid'] . " and g.id='$id'");		
 			
 			if ($op == 1) {
 				if ($sell['zfbfile']) $payment[] = array('name' => "支付宝", 'type' => 'zfb');
@@ -948,6 +948,7 @@ class Androidapi_EweiShopV2Page extends MobilePage
 				if ($sell['wxfile2']) $payment[] = array('name' => "微信", 'type' => 'wx');
 				if ($sell['bank2'] && $sell['bankid2'] && $sell['bankname2']) $payment[] = array('name' => "银行", 'type' => 'bank');
 			}
+			
 	
 			// if ($sell['openid'] == $_W['openid']) {
 			// 	$type = 1;
@@ -1012,11 +1013,11 @@ class Androidapi_EweiShopV2Page extends MobilePage
 					$op = $_GPC['op'];
 					//判断该用户是否有足够的币进行抢单
 					$member = m('member')->getMember($_W['openid'], true);
-					$sell   = pdo_fetch("select g.trx,m.mobile,m2.mobile as mobile2 from" . tablename("guamai") . ' g left join ' . tablename('ewei_shop_member') . ' m ON m.openid=g.openid ' . ' left join ' . tablename('ewei_shop_member') . ' m2 ON m2.openid=g.openid2 ' . " where g.uniacid=" . $_W['uniacid'] . " and g.id='$id' and g.type=0");
+					$sell   = pdo_fetch("select g.trx,m.mobile,g.openid,m2.mobile as mobile2 from" . tablename("guamai") . ' g left join ' . tablename('ewei_shop_member') . ' m ON m.openid=g.openid ' . ' left join ' . tablename('ewei_shop_member') . ' m2 ON m2.openid=g.openid2 ' . " where g.uniacid=" . $_W['uniacid'] . " and g.id='$id' and g.type=1");
 					if ($guamai_nums >= 1) {
 						returnJson([],"您还有订单尚未处理或还在交易中,请先进行交易！",-2);
 					}
-
+					
 					if(empty($sell['openid'])){
 						returnJson([],"订单出错", 0);
 					}
@@ -1025,7 +1026,6 @@ class Androidapi_EweiShopV2Page extends MobilePage
 						returnJson([],"不能卖出自己发放的账单", 0);
 					}
 					
-					// returnJson(['openid' => $sell['openid'] , 'openid2' => $_W['openid']]);
 
 					//判断该会员是否上传收款信息
 					if (!$member['zfbfile'] && !$member['wxfile'] && (!$member['bankid'] || !$member['bankname'] || !$member['bank'])) {
