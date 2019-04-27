8aca4f769a81d1e98a8f7bedbd486274ef3415e3
xiaozhi9752 <975247129@qq.com>
2019-04-27 15:04:14 +0800

    分页

--- addons/ewei_shopv2/core/mobile/member/androidapi.php
+++ addons/ewei_shopv2/core/mobile/member/androidapi.php
@@ -2269,12 +2269,12 @@ class Androidapi_EweiShopV2Page extends MobilePage
 		$offset = ($pindex - 1) * $psize;
 		$list =  pdo_fetchall("select g.*,m.nickname from" . tablename("ewei_shop_member_log") . "g left join" . tablename("ewei_shop_member") . "m on g.openid=m.openid" . " where g.openid='$openid' order by g.createtime desc limit $offset,$psize");
 		$zhuanzhang = pdo_fetchall("select g.*,m.nickname from" . tablename("ewei_zhuanzhang") . "g left join" . tablename("ewei_shop_member") . "m on g.openid=m.openid" . " where g.openid='$openid' order by g.createtime desc limit $offset,$psize");
-		foreach ($zhuanzhang as $k => $v) {
-			$zhuanzhang[$k]['openid'] = substr($v['openid'], -11);
-			$zhuanzhang[$k]['openid2'] = substr($v['openid2'], -11);
-			$zhuanzhang[$k]['createtime'] = date("Y-m-d", $v['createtime']);
-		}
-		if( !empty($list) ){
+		if( !empty($list) || !empty($zhuanzhang) ){
+			foreach ($zhuanzhang as $k => $v) {
+				$zhuanzhang[$k]['openid'] = substr($v['openid'], -11);
+				$zhuanzhang[$k]['openid2'] = substr($v['openid2'], -11);
+				$zhuanzhang[$k]['createtime'] = date("Y-m-d", $v['createtime']);
+			}
 			foreach ($list as $key => $val) {
 				$list[$key]['shouxufei'] = $val['money1'] - $val['money'];
 				$list[$key]['createtime'] = date("Y-m-d", $val['createtime']);
