bb392876d0ea09ded32874570d2bf9a46455b8e7
unknown <sonelyx>
2019-05-09 17:20:21 +0800

    boot

--- web/index.php
+++ web/index.php
@@ -209,11 +209,7 @@ if (is_array($acl[$controller]['founder']) && in_array($action, $acl[$controller
 if (user_is_vice_founder($_W['uid']) && is_array($acl[$controller]['vice-founder']) && !in_array($action, $acl[$controller]['vice-founder'])) {
 	message('不能访问, 需要相应的权限才能访问.');
 }
-// checklogin();
-// echo 123123;
-// print_r($controller);
-// print_r($action);
-// die;
+checklogin();
 require _forward($controller, $action);
 
 define('ENDTIME', microtime());
