0888b284762704871cf477f8dabdedc0eb2d2723
xiaozhi9752 <975247129@qq.com>
2019-05-24 18:11:18 +0800

    1

--- addons/ewei_shopv2/core/mobile/member/androidapi.php
+++ addons/ewei_shopv2/core/mobile/member/androidapi.php
@@ -1759,13 +1759,13 @@ class Androidapi_EweiShopV2Page extends MobilePage
 		
 		if ($type == 4) {		//积分记录
 
-			$list =  pdo_fetchall("select g.*,m.nickname from" . tablename("ewei_shop_receive_hongbao") . "g left join" . tablename("ewei_shop_member") . "m on g.openid=m.openid" . " where g.uniacid=:uniacid  and g.openid=:openid order by g.time desc", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
+			$list =  pdo_fetchall("select g.*,m.nickname from " . tablename("ewei_shop_receive_hongbao") . " g left join " . tablename("ewei_shop_member") . " m on g.openid=m.openid" . " where g.uniacid=:uniacid  and g.openid=:openid order by g.time desc", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
 
 			foreach ($list as $key => $val) {
 				$list[$key]['createtime'] = date("Y-m-d H:i:s", $val['time']);
 			}
 
-			$count =  pdo_fetch("select sum(g.money) as money,sum(g.money2) as money2 from" . tablename("ewei_shop_receive_hongbao") . "g left join" . tablename("ewei_shop_member") . "m on g.openid=m.openid" . " where g.uniacid=:uniacid  and g.openid=:openid", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
+			$count =  pdo_fetch("select sum(g.money) as money,sum(g.money2) as money2 from " . tablename("ewei_shop_receive_hongbao") . " g left join " . tablename("ewei_shop_member") . " m on g.openid=m.openid" . " where g.uniacid=:uniacid  and g.openid=:openid", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
 
 			if (!$count['money'] && !$count['money2']) {
 				$summoeny = 0;
@@ -1778,13 +1778,13 @@ class Androidapi_EweiShopV2Page extends MobilePage
 			returnJson($data);
 		}
 
-		$list =  pdo_fetchall("select g.*,m.nickname from" . tablename("ewei_shop_order_goods1") . "g left join" . tablename("ewei_shop_member") . "m on g.openid2=m.openid" . " where g.uniacid=:uniacid and g.type='$type' and g.openid=:openid order by g.createtime desc", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
+		$list =  pdo_fetchall("select g.*,m.nickname from " . tablename("ewei_shop_order_goods1") . " g left join " . tablename("ewei_shop_member") . " m on g.openid2=m.openid" . " where g.uniacid=:uniacid and g.type='$type' and g.openid=:openid order by g.createtime desc", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
 
 		foreach ($list as $key => $val) {
 			$list[$key]['createtime'] = date("Y-m-d H:i:s", $val['createtime']);
 		}
 
-		$count =  pdo_fetch("select sum(g.money) as money,sum(g.money2) as money2 from" . tablename("ewei_shop_order_goods1") . "g left join" . tablename("ewei_shop_member") . "m on g.openid2=m.openid" . " where g.uniacid=:uniacid and g.type='$type' and g.openid=:openid", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
+		$count =  pdo_fetch("select sum(g.money) as money,sum(g.money2) as money2 from " . tablename("ewei_shop_order_goods1") . " g left join " . tablename("ewei_shop_member") . " m on g.openid2=m.openid" . " where g.uniacid=:uniacid and g.type='$type' and g.openid=:openid", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
 
 		$data = array('list' => $list, 'money' => $count['money'] + $count['money2']);
 
@@ -1803,13 +1803,13 @@ class Androidapi_EweiShopV2Page extends MobilePage
 
 		if ($type == 4) {		//积分记录
 
-			$list =  pdo_fetchall("select g.*,m.nickname from" . tablename("ewei_shop_receive_hongbao") . "g left join" . tablename("ewei_shop_member") . "m on g.openid=m.openid" . " where g.uniacid=:uniacid and g.time>=$start and g.time<=$end and g.openid=:openid order by g.time desc", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
+			$list =  pdo_fetchall("select g.*,m.nickname from " . tablename("ewei_shop_receive_hongbao") . " g left join " . tablename("ewei_shop_member") . " m on g.openid=m.openid" . " where g.uniacid=:uniacid and g.time>=$start and g.time<=$end and g.openid=:openid order by g.time desc", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
 
 			foreach ($list as $key => $val) {
 				$list[$key]['createtime'] = date("Y-m-d H:i:s", $val['time']);
 			}
 
-			$count =  pdo_fetch("select sum(g.money) as money,sum(g.money2) as money2 from" . tablename("ewei_shop_receive_hongbao") . "g left join" . tablename("ewei_shop_member") . "m on g.openid=m.openid" . " where g.uniacid=:uniacid and g.time>=$start and g.time<=$end  and g.openid=:openid", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
+			$count =  pdo_fetch("select sum(g.money) as money,sum(g.money2) as money2 from " . tablename("ewei_shop_receive_hongbao") . "g left join " . tablename("ewei_shop_member") . " m on g.openid=m.openid" . " where g.uniacid=:uniacid and g.time>=$start and g.time<=$end  and g.openid=:openid", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
 
 			if (!$count['money'] && !$count['money2']) {
 				$summoeny = 0;
@@ -1822,13 +1822,13 @@ class Androidapi_EweiShopV2Page extends MobilePage
 			returnJson($data);
 		}
 
-		$list =  pdo_fetchall("select g.*,m.nickname from" . tablename("ewei_shop_order_goods1") . "g left join" . tablename("ewei_shop_member") . "m on g.openid2=m.openid" . " where g.uniacid=:uniacid and g.createtime>=$start and g.createtime<=$end and g.type='$type' and g.openid=:openid order by g.createtime desc", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
+		$list =  pdo_fetchall("select g.*,m.nickname from " . tablename("ewei_shop_order_goods1") . " g left join " . tablename("ewei_shop_member") . " m on g.openid2=m.openid" . " where g.uniacid=:uniacid and g.createtime>=$start and g.createtime<=$end and g.type='$type' and g.openid=:openid order by g.createtime desc", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
 
 		foreach ($list as $key => $val) {
 			$list[$key]['createtime'] = date("Y-m-d H:i:s", $val['createtime']);
 		}
 
-		$count =  pdo_fetch("select sum(g.money) as money,sum(g.money2) as money2  from" . tablename("ewei_shop_order_goods1") . "g left join" . tablename("ewei_shop_member") . "m on g.openid2=m.openid" . " where g.uniacid=:uniacid and g.createtime>=$start and g.createtime<=$end and g.type='$type' and g.openid=:openid", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
+		$count =  pdo_fetch("select sum(g.money) as money,sum(g.money2) as money2  from " . tablename("ewei_shop_order_goods1") . " g left join " . tablename("ewei_shop_member") . " m on g.openid2=m.openid" . " where g.uniacid=:uniacid and g.createtime>=$start and g.createtime<=$end and g.type='$type' and g.openid=:openid", array(':uniacid' => $_W['uniacid'], ':openid' => $_W['openid']));
 
 		$data = array('list' => $list, 'money' => $count['money'] + $count['money2']);
 
